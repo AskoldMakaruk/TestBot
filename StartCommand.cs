a5fe08cf75746d80654fb60ce080855e8a917385
AskoldMakaruk <askoldmakaruk@gmail.com>
2019-11-11 14:54:25 +0200

    init

---
+++ StartCommand.cs
@@ -0,0 +1,17 @@
+﻿using BotFramework;
+using BotFramework.Bot;
+using BotFramework.Commands;
+using Telegram.Bot.Types;
+
+namespace TestBot
+{
+    public class StartCommand : StaticCommand
+    {
+        public override Response Execute(Message message, Client client)
+        {
+            return new Response(this).SendTextMessage(message.From.Id, "I'm alive!");
+        }
+
+        public override string Alias => "/start";
+    }
+}
